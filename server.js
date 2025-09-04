26289e9a7e1f1845fd2244d203e67d637823e4b2
arjsbar4062-design <arjsbar4062@students.uplifteducation.org>
2025-09-04 16:13:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -100,7 +100,8 @@ app.use(express.static(path.join(__dirname, "public")));
 // seed bookmarks if empty
 seedBookmarks(db);
 
+// --- Start server on Render ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
